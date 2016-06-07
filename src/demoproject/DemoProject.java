d3b63a8c67afde52122ca8d91b0e6916448c9217
macbookoleg <macbookoleg@192.168.0.11>
2016-06-07 17:16:13 +0200

    clean main java class changed label for search button

--- src/demoproject/DemoProject.java
+++ src/demoproject/DemoProject.java
@@ -5,28 +5,9 @@
  */
 package demoproject;
 
-import demoproject.Formular.AuftragController;
 import demoproject.Formular.MenuController;
-import java.io.IOException;
 import java.sql.Connection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -36,7 +17,7 @@ import javafx.stage.Stage;
 public class DemoProject extends Application {
 
 	@Override
-	public void start(Stage primaryStage) throws IOException, Exception {
+	public void start(Stage primaryStage) throws Exception {
 		MenuController mController = new MenuController();
 		mController.start(primaryStage);
 	}
